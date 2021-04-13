3c1fd65b4070336ec6600a13c2a800ccef30fcfc
LizLaffitte <laffitte.digital@gmail.com>
2021-04-13 11:31:47 -0400

    Initial commit

---
+++ spec/search_tracker_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SearchTracker do
+  it "has a version number" do
+    expect(SearchTracker::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
